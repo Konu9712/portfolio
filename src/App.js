c2cd29447e3d152afcfb77575489b03146b07611
Konu <51238256+Konu9712@users.noreply.github.com>
2023-06-21 19:43:53 -0400

    SideBar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import logo from "./logo.svg";
-import "./App.css";
+import React from "react";
+import Sidebar from "./Components/SideBar/SideBar";
 
-function App() {
+export default function MacbookPro161() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>First PR</p>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-5 sidebar" style={{ marginTop: "10%" }}>
+          <Sidebar />
+        </div>
+        <div className="col-lg-7 mainContainer">mainTab</div>
+      </div>
     </div>
   );
 }
-
-export default App;
